f5a277f92cb8102799a5addfc9e27a8dfaf195a6
user04 <user04@ws004-pc.(none)>
2012-04-20 19:08:43 +0400

    Module options made and works

--- install/index.php
+++ install/index.php
@@ -1,15 +1,22 @@
-<?
+<?php
 Class imageimport extends CModule {
 
 	public $MODULE_ID = 'imageimport';
-	public $MODULE_VERSION = '00.00.01';
-	public $MODULE_VERSION_DATE = '2012-03-22 12:30:00';
+	public $MODULE_VERSION = '11.0.04';
+	public $MODULE_VERSION_DATE = '2012-04-20 19:00:00';
 	public $MODULE_NAME = 'Image Import';
 	public $MODULE_DESCRIPTION = 'Folder image import';
-	public $MODULE_GROUP_RIGHTS;
+	public $MODULE_GROUP_RIGHTS = 'Y';
 
 	public function __construct() {}
-	public function DoInstall() {}
-	public function DoUninstall() {}
+	public function DoInstall() {
+		RegisterModule('imageimport');
+		return true;
+	}
+	public function DoUninstall() {
+		COption::RemoveOption('imageimport');
+		UnRegisterModule('imageimport');
+		return true;
+	}
 }
 ?>
--- options.php
+++ options.php
@@ -1,40 +1,212 @@
 <?
 if (!$USER->IsAdmin()) return;
 
+IncludeModuleLangFile(__FILE__);
 
-$aTab = array(
-	array("DIV" => "edit1", "TAB" => 'Настройки импорта изображений', "ICON" => "perfmon_settings", "TITLE" => 'Настройки импорта изображений',),
-);
-$tabControl = new CAdminTabControl("tabControl", $aTabs);
+// print '<pre>';
+// print_r($_POST);
+// print '</pre>';
+if ($REQUEST_METHOD == 'POST') {
+	COption::SetOptionString('imageimport', 'search_dir', $_POST['search_dir'], false);
+	COption::SetOptionString('imageimport', 'rel_dir', $_POST['rel_dir'], false);
+	COption::SetOptionString('imageimport', 'target', $_POST['target'], false);
+	COption::SetOptionString('imageimport', 'recoursive', ($_POST['recoursive']=='on')?'Y':'N', false);
+	COption::SetOptionString('imageimport', 'clear_after', ($_POST['clear_after']=='on')?'Y':'N', false);
 
-if ($_SERVER['REQUIEST_METHOD'] == 'POST' && check_bitrix_sessid()) {
-	//hadle post
+	COption::SetOptionString('imageimport', 'iblock_type', $_POST['iblock_type_id'], false);
+	COption::SetOptionString('imageimport', 'iblock', $_POST['select-' . $_POST['iblock_type_id']], false);
+
+	CAdminMessage::ShowMessage(array(
+		'MESSAGE' => GetMessage('II_OPT_SAVED_OK_TITLE'),
+		'DETAILS' => GetMessage('II_OPT_SAVED_OK_MSG'),
+		'TYPE' => 'OK',
+		'HTML' => false,
+	));
 }
 
+
+		// CAdminMessage::ShowMessage(array(
+		// 	"MESSAGE" => $status_title,
+		// 	"DETAILS" =>  $status_msg,
+		// 	"TYPE" => "OK",
+		// 	"HTML" => true));
+
+
+$iblock_saved = COption::GetOptionString('imageimport', 'iblock', '0');
+$rel_dir = COption::GetOptionString('imageimport', 'rel_dir', 'document');
+$target = COption::GetOptionString('imageimport', 'target', 'iblock');
+
+
+if (!CModule::IncludeModule('iblock')) return;
+
+$rIBlockTypeList = CIBlockType::GetList(
+	array('SORT' => 'ASC'),
+	array()
+);
+$arIBlockTypeList = array();
+while ($iblock_type = $rIBlockTypeList->GetNext()) $arIBlockTypeList[] = $iblock_type;
+foreach($arIBlockTypeList as $i => $iblock_type) {
+	$iblock_lang_settings = CIBlockType::GetByIDLang($iblock_type['ID'], LANGUAGE_ID, true);
+	$arIBlockTypeList[$i]['NAME'] = $iblock_lang_settings['NAME'];
+}
+
+
+$rIBlockList = CIBlock::GetList(
+	array('SORT' => 'ASC'),
+	array(),
+	false
+);
+$arIBlockList = array();
+while ($iblock = $rIBlockList->GetNext()) $arIBlockList[] = $iblock; 
+
+
+$aTabs = array(
+	array(
+		'DIV' => 'edit-options-common',
+		'TAB' => GetMessage('II_OPT_COMMON_NAME'),
+		'ICON' => '',
+		'TITLE' => GetMessage('II_OPT_COMMON_TITLE'),
+	),
+	array(
+		"DIV" => 'edit-options-iblock', 
+		"TAB" => GetMessage('II_OPT_IB_NAME'), 
+		"ICON" => '', 
+		"TITLE" => GetMessage('II_OPT_IB_TITLE'),
+	),
+	array(
+		'DIV' => 'edit-options-medialibrary',
+		'TAB' => GetMessage('II_OPT_ML_NAME'),
+		'ICON' => '',
+		'TITLE' => GetMessage('II_OPT_ML_TITLE'),
+	),
+);
+
+
+$tabControl = new CAdminTabControl("tabControl", $aTabs);
 $tabControl->Begin(); ?>
 <form method="post" action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=urlencode($mid)?>&amp;lang=<?echo LANGUAGE_ID?>">
 <? $tabControl->BeginNextTab(); ?>
 
-	<label for="search_dir">Папка, из которой будут загружаться изображения: </label>
-	<input type="text" name="" value="<? COption::GetOptionString('iblock', 'search_dir', 'image_import'); ?>" />
+<tr class="heading">
+	<td colspan="2" align="center">
+		<?=GetMessage('II_OPT_HEADING_COMMON_IMPORT')?>
+	</td>
+</tr>
 
-	<label for="iblock_id">Информационный блок для привязки изображений</label>
-	<select name='iblock_id'>
-		<? foreach(array('1' => 'Первый инфоблок', '2' => 'Второй инфоблок',) as $key => $text): ?>
-		<option value="<?=$key?>"><?=$text?></option>
+<tr>
+	<td valign='top'>
+		<label for="search_dir"><?=GetMessage('II_OPT_LABEL_DIR')?>: </label>
+	</td>
+	<td>
+		<input type="text" name="search_dir" value="<?=COption::GetOptionString('imageimport', 'search_dir', '')?>" /><br />
+	</td>
+</tr>
+
+<tr>
+	<td>
+		&nbsp;
+	</td>
+	<td>
+		<label><input type="radio" name ="rel_dir" value="document" <? if ($rel_dir == 'document'):?>checked="checked" <? endif; ?>/><?=GetMessage('II_OPT_REL_DIR_DOCUMENT')?></label><br />
+		<label><input type="radio" name ="rel_dir" value="server" <? if ($rel_dir == 'server'):?>checked="checked" <? endif; ?>/><?=GetMessage('II_OPT_REL_DIR_SERVER')?></label><br />
+	</td>
+</tr>
+
+<tr>
+	<td>
+		&nbsp;
+	</td>
+	<td>
+		<label><input type="checkbox" name="recoursive" <? if (COption::GetOptionString('imageimport', 'recoursive', 'N') == 'Y'):?>checked="checked"<? endif; ?>/><?=GetMessage('II_OPT_RECOURSIVE')?></label>
+	</td>
+</tr>
+
+<tr>
+	<td>
+		&nbsp;
+	</td>
+	<td>
+		<label><input type="checkbox" name="clear_after" <? if (COption::GetOptionString('imageimport', 'clear_after', 'Y') == 'Y'):?>checked="checked"<? endif; ?>/><?=GetMessage('II_OPT_CLEAR')?></label>
+	</td>
+</tr>
+
+<tr class="heading">
+	<td colspan="2" align="center">
+		<?=GetMessage('II_OPT_HEADING_COMMON_TARGET')?>
+	</td>
+</tr>
+
+<tr>
+	<td>
+		&nbsp;
+	</td>
+	<td>
+		<label><input type="radio" name ="target" value="iblock" <? if ($target == 'iblock'):?>checked="checked" <? endif; ?>/><?=GetMessage('II_OPT_TARGER_IBLOCK')?></label><br />
+		<label><input type="radio" name ="target" value="medialib" <? if ($target == 'medialib'):?>checked="checked" <? endif; ?>/><?=GetMessage('II_OPT_REL_TARGET_MEDIALIB')?></label><br />
+	</td>
+</tr>
+
+<? $tabControl->BeginNextTab(); ?>
+
+<tr>
+	<td>
+		<label for='iblock_type_id'><?=GetMessage('II_OPT_LABEL_TYPE')?></label>
+	</td>
+	<td>
+		<select name='iblock_type_id' id='iblock_type_id'>
+				<option value="notype"></option>
+			<? foreach($arIBlockTypeList as $iblock_type): ?>
+				<option value="<?=$iblock_type['ID']?>"><?=$iblock_type['NAME']?></option>
+			<? endforeach; ?>
+		</select>
+	</td>
+</tr>
+
+<tr>
+	<td>
+		<label for="iblock_id"><?=GetMessage('II_OPT_LABEL_IBLOCK')?></label>
+	</td>
+	<td>
+		<select name='select-notype' id="select-notype" style="font-style:italic;">
+				<option value="0"><?=GetMessage('II_OPT_SELECT_IBLOCK_TYPE_FIRST')?></option>
+		</select>
+		<? foreach($arIBlockTypeList as $iblock_type): ?>
+		<select name="select-<?=$iblock_type['ID']?>" id="select-<?=$iblock_type['ID']?>">
+			<? foreach ($arIBlockList as $iblock): ?>
+				<? if ($iblock_type['ID'] == $iblock['IBLOCK_TYPE_ID']): ?>
+				<option value="<?=$iblock['ID']?>"<? if ($iblock_saved == $iblock['ID']): ?> selected="selected"<? endif; ?>><?=$iblock['NAME']?></option>
+				<? endif; ?>
+			<? endforeach; ?>
+		</select>
 		<? endforeach; ?>
-	</select>
+	</td>
+</tr>
 
-	<label for="section_id">Раздел для привязки изоражений</label>
-	<select name="section_id">
-		<? foreach (array('1' => 'Первый раздел', '2' => 'Второй раздел',) as $key => $text): ?>
-		<option value="<?=$key?>"><?=$text?></option>
-		<? endforeach; ?>
-	</select>
+<script type="text/javascript">
+var ii_select_classes = [];
+ii_select_classes.push('select-notype');
+<? foreach($arIBlockTypeList as $iblock_type): ?>
+ii_select_classes.push('select-' + '<?=$iblock_type['ID']?>');
+<? endforeach; ?>
 
-	<input type="submit" name="submit" value="Сохранить" />
-	<input type="button" name="defaults" value="По умолчанию" />
+var ii_type_select = document.getElementById('iblock_type_id');
+ii_type_select.value = '<?=COption::GetOptionString('imageimport', 'iblock_type', 'notype')?>';
 
+function iiSwitchSelect() {
+	for(var i = 0; i < ii_select_classes.length; i++) {
+		var current_select = document.getElementById(ii_select_classes[i]);
+		current_select.style.display = 'none';
+	}
+	var current_select = document.getElementById('select-' + ii_type_select.value);
+	current_select.style.display = 'block';
+}
+
+iiSwitchSelect();
+ii_type_select.onchange = iiSwitchSelect;
+</script>
+
+<? $tabControl->BeginNextTab(); ?>
 <? $tabControl->Buttons(); ?>
-<? $tabControl->End(); ?>
+	<input type="submit" name="submit" value="<?=GetMessage('II_OPT_SAVE')?>" />
 </form>
+<? $tabControl->End(); ?>
