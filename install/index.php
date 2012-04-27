3dc73a5e47bcae8540d97053d6bb9986edd161de
user04 <clesign@yandex.ru>
2012-04-27 11:52:49 +0400

    Version handler added.

--- install/index.php
+++ install/index.php
@@ -2,13 +2,17 @@
 Class imageimport extends CModule {
 
 	public $MODULE_ID = 'imageimport';
-	public $MODULE_VERSION = '11.0.06';
-	public $MODULE_VERSION_DATE = '2012-04-26 16:00:00';
+	public $MODULE_VERSION;
+	public $MODULE_VERSION_DATE;
 	public $MODULE_NAME = 'Image Import';
 	public $MODULE_DESCRIPTION = 'Folder image import';
 	public $MODULE_GROUP_RIGHTS = 'Y';
 
-	public function __construct() {}
+	public function __construct() {
+		include(dirname(__FILE__) . '/version.php');
+		$this->MODULE_VERSION = $arModuleVersion['VERSION'];
+		$this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
+	}
 	public function DoInstall() {
 		RegisterModule('imageimport');
 		CopyDirFiles($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/imageimport/install/admin', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin');
---
+++ install/version.php
@@ -0,0 +1,6 @@
+<?php
+$arModuleVersion = array(
+	'VERSION' => '11.0.07',
+	'VERSION_DATE' => '2012-04-27 11:00:00',
+);
+?>
